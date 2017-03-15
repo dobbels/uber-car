667f531b6efab961b949c10edc14fafdfe669048
Unknown <steven.dobbelaere@student.kuleuven.be>
2017-03-15 22:56:58 +0100

    Add todo

--- car/Main.java
+++ car/Main.java
@@ -1,8 +1,8 @@
 
 public class Main {
 	public static void Main(String[] args){
-		Car carObject = new Car("Renault", "Megane", "ABC-123", new Location(0,0), 1);
+		Car car = new Car("Renault", "Megane", "ABC-123", new Location(0,0), 1);
 		
-
+		//TODO at the start of everything run car.register() and if it returns false, repeat the registration or print mistake or something
 	}
 }
