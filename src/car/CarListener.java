5dcb5f25cd8569abee3271cf7ccf4c48aaa6acd6
Unknown <steven.dobbelaere@student.kuleuven.be>
2017-04-22 12:54:04 +0200

    uncomment errors

--- src/car/CarListener.java
+++ src/car/CarListener.java
@@ -1,22 +1,22 @@
-//package car;
-//
-//import java.io.IOException;
-//
-//import javax.servlet.ServletContextEvent;
-//
-//public class CarListener implements javax.servlet.ServletContextListener {
-//
-//	@Override
-//	public void contextDestroyed(ServletContextEvent arg0) {
-//		// TODO Auto-generated method stub
-//
-//	}
-//
-//	@Override
-//	public void contextInitialized(ServletContextEvent arg0) {
-//		// TODO Auto-generated method stub
-//		MainThread t = new MainThread();
-//		new Thread(t).start();
-//	}
-//
-//}
+package car;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContextEvent;
+
+public class CarListener implements javax.servlet.ServletContextListener {
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+		MainThread t = new MainThread();
+		new Thread(t).start();
+	}
+
+}
