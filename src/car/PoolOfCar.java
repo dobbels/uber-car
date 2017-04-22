a6f6d104d9612b575b99fce64c4e2c0eb0300e31
Unknown <steven.dobbelaere@student.kuleuven.be>
2017-04-22 12:51:50 +0200

    Write handler for car registration file

--- src/car/PoolOfCar.java
+++ src/car/PoolOfCar.java
@@ -3,14 +3,14 @@ package car;
 public class PoolOfCar {
 	
 	static Car[] cars =  {
-		new Car("Renault", "Megane", "ABC-121", new Location(40.437056,-3.706269), 1),
-		new Car("Renault", "Megane", "ABC-122", new Location(40.437056,-3.706269), 1),
-		new Car("Renault", "Megane", "ABC-123", new Location(40.437056,-3.706269), 1),
-		new Car("Renault", "Megane", "ABC-123", new Location(40.437056,-3.706269), 1),
-		new Car("Renault", "Megane", "ABC-124", new Location(40.437056,-3.706269), 1),
-		new Car("Renault", "Megane", "ABC-125", new Location(40.437056,-3.706269), 1),
-		new Car("Renault", "Megane", "ABC-126", new Location(40.437056,-3.706269), 1),
-		new Car("Renault", "Megane", "ABC-127", new Location(40.437056,-3.706269), 1)
+			new Car("Renault", "Megane", "ABC-121", new Location(40.437056,-3.706269), 1),
+			new Car("Renault", "Megane", "ABC-122", new Location(40.437056,-3.706269), 2),
+			new Car("Renault", "Megane", "ABC-123", new Location(40.437056,-3.706269), 3),
+			new Car("Renault", "Megane", "ABC-124", new Location(40.437056,-3.706269), 4),
+			new Car("Renault", "Megane", "ABC-125", new Location(40.437056,-3.706269), 5),
+			new Car("Renault", "Megane", "ABC-126", new Location(40.437056,-3.706269), 6),
+			new Car("Renault", "Megane", "ABC-127", new Location(40.437056,-3.706269), 7),
+			new Car("Renault", "Megane", "ABC-128", new Location(40.437056,-3.706269), 8)
 	};
 	
 
--- src/car_design/XmlServlet.java
+++ src/car_design/XmlServlet.java
@@ -1,87 +1,87 @@
-package car_design;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.json.JSONException;                         
-import org.json.JSONObject;                        //Using JSONObject class
-
-import car.*;
-
-public class XmlServlet extends HttpServlet {	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//doPost(request, response);         //Automatically calls doPost() method
-	}
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		BufferedReader reader = new BufferedReader(new InputStreamReader(request.getInputStream()));  
-		String str ="";                                   
-		String line;
-		while (true) {                            //Does the reading operation until the end
-			line = reader.readLine();
-			if (line == null) {
-				break;
-			}
-			str += line;
-		}
-		reader.close();
-		JSONObject jsonObj;
-		int myId = 0; int myCarId = 0;
-		double myLat = -1, myLon = -1, myLat2 = -1, myLon2 = -1;   
-		System.out.println(str);
-		try {
-			 jsonObj = new JSONObject(str);     //JSONObject contains the received data
-			 myId = jsonObj.getInt("trip_id");       //Gets the value of "id" key from JSONObject
-			 myCarId = jsonObj.getInt("car_id");
-			 myLat = jsonObj.getDouble("lat");
-			 myLon = jsonObj.getDouble("lon");
-			 myLat2 = jsonObj.getDouble("lat2");
-			 myLon2 = jsonObj.getDouble("lon2");
-		} catch (JSONException e1) {
-			// TODO Auto-generated catch block
-			System.out.println("Exception is detected!");
-			e1.printStackTrace();
-		} 
-		
-		// If car doesn't exist, send error message (with specific code 410)
-		// If it exists, look in pool of car, check if it's free on index "car_id" - 1 
-		//   and (1) add event to the right thread (2) add event to a single main thread but with argument "car_id".
-		// Create array of simulators and threads or implement multiple cars in one simulator (in main thread java)
-		
-		Car c = PoolOfCar.getCar();
-		System.out.println(c);
-		
-		if (c.isFree()) {
-			System.out.println("Dispatch is recognized successfully!");
-			System.out.println("myId is: " + myId);
-			System.out.println("myCarId is: " + myCarId);
-			System.out.println("myLat is: " + myLat);
-			System.out.println("myLon is: "+ myLon);
-			System.out.println("myLat2 is: "+ myLat2);
-			System.out.println("myLon2 is: " +myLon2);
-			response.setStatus(200);
-			
-			Trip trip = new Trip(myId, new Location(myLat,myLon), new Location(myLat2,myLon2));
-			c.setTrip(trip);
-			CarToPassenger ctp = new CarToPassenger();
-			ctp.car = c;
-			MainThread.s.addEvent(ctp);
-			response.getWriter().println("OK");
-		}
-		else{
-			System.out.println("Error! Something went wrong!");
-			//c.passengerGetOut();
-			response.setStatus(400);
-			response.getWriter().println("Car is not available");
-		}	
-	}			
-}
+//package car_design;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.ArrayList;
+//
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//import org.json.JSONException;
+//import org.json.JSONObject;                        //Using JSONObject class
+//
+//import car.*;
+//
+//public class XmlServlet extends HttpServlet {
+//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		//doPost(request, response);         //Automatically calls doPost() method
+//	}
+//
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//
+//		BufferedReader reader = new BufferedReader(new InputStreamReader(request.getInputStream()));
+//		String str ="";
+//		String line;
+//		while (true) {                            //Does the reading operation until the end
+//			line = reader.readLine();
+//			if (line == null) {
+//				break;
+//			}
+//			str += line;
+//		}
+//		reader.close();
+//		JSONObject jsonObj;
+//		int myId = 0; int myCarId = 0;
+//		double myLat = -1, myLon = -1, myLat2 = -1, myLon2 = -1;
+//		System.out.println(str);
+//		try {
+//			 jsonObj = new JSONObject(str);     //JSONObject contains the received data
+//			 myId = jsonObj.getInt("trip_id");       //Gets the value of "id" key from JSONObject
+//			 myCarId = jsonObj.getInt("car_id");
+//			 myLat = jsonObj.getDouble("lat");
+//			 myLon = jsonObj.getDouble("lon");
+//			 myLat2 = jsonObj.getDouble("lat2");
+//			 myLon2 = jsonObj.getDouble("lon2");
+//		} catch (JSONException e1) {
+//			// TODO Auto-generated catch block
+//			System.out.println("Exception is detected!");
+//			e1.printStackTrace();
+//		}
+//
+//		// If car doesn't exist, send error message (with specific code 410)
+//		// If it exists, look in pool of car, check if it's free on index "car_id" - 1
+//		//   and (1) add event to the right thread (2) add event to a single main thread but with argument "car_id".
+//		// Create array of simulators and threads or implement multiple cars in one simulator (in main thread java)
+//
+//		Car c = PoolOfCar.getCar();
+//		System.out.println(c);
+//
+//		if (c.isFree()) {
+//			System.out.println("Dispatch is recognized successfully!");
+//			System.out.println("myId is: " + myId);
+//			System.out.println("myCarId is: " + myCarId);
+//			System.out.println("myLat is: " + myLat);
+//			System.out.println("myLon is: "+ myLon);
+//			System.out.println("myLat2 is: "+ myLat2);
+//			System.out.println("myLon2 is: " +myLon2);
+//			response.setStatus(200);
+//
+//			Trip trip = new Trip(myId, new Location(myLat,myLon), new Location(myLat2,myLon2));
+//			c.setTrip(trip);
+//			CarToPassenger ctp = new CarToPassenger();
+//			ctp.car = c;
+//			MainThread.s.addEvent(ctp);
+//			response.getWriter().println("OK");
+//		}
+//		else{
+//			System.out.println("Error! Something went wrong!");
+//			//c.passengerGetOut();
+//			response.setStatus(400);
+//			response.getWriter().println("Car is not available");
+//		}
+//	}
+//}
