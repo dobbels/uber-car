773f9a899e68cff0f40df1e420c26d5da92b1f3b
SagarLakh <djlakh@gmail.com>
2017-04-23 16:50:21 +0200

    idea for logout

--- src/car/CarListener.java
+++ src/car/CarListener.java
@@ -10,10 +10,12 @@ public class CarListener implements javax.servlet.ServletContextListener {
 	@Override
 	public void contextDestroyed(ServletContextEvent arg0) {
 
+		//TODO first close down threads and then logout
         //TODO use this to log out cars ??
         //TODO make sure that tomcat server is closed every time by opening the Servers view and rightclicking the server instance and choosing Stop, or by hitting the red button in the Servers view whose tooltip says Stop the server.
         // This method will not be called when you terminate the server altogether by hitting the red button in the Console view whose tooltip says Terminate. Terminating (killing) is not the same as stopping.
-
+		System.out.println("stopped");
+		
 	}
 
 	@Override
